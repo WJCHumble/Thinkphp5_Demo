9e73027ddacbb0ab1bd873daa5e07d2f336b52b6
WJCHumble <1957503379@qq.com>
2020-09-29 09:09:12 +0800

    fix: 删除个人账号

--- tp5/application/extra/aliyun.php
+++ tp5/application/extra/aliyun.php
@@ -1,8 +1,8 @@
 <?php
 return [
-  "OSS_ACCESS_ID" => 'LTAIPo3k5hYFFlbB',
-  "OSS_ACCESS_KEY" => 'Y4UFtNGjdfluxAO8jxiH5nf5UrkTJ3',
-  "OSS_ENDPOINT" => 'oss-cn-beijing.aliyuncs.com',
-  "OSS_TEST_BUCKET" => 'wuzhiwei',
+  "OSS_ACCESS_ID" => '',
+  "OSS_ACCESS_KEY" => '',
+  "OSS_ENDPOINT" => '',
+  "OSS_TEST_BUCKET" => '',
 ];
  ?>
